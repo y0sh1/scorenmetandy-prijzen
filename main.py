3b11715d4eeb9dbc2fb77b01cf8397674ec8b1fc
Y0sh1 <joshua@theinternets.nl>
2025-03-08 15:55:28 +0100

    Fix Type

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def extract_fuel_prices(url):
     fuel_prices = {fuel: float(price.replace(',', '.')) for fuel, price in matches}
     json_output = json.dumps(fuel_prices, indent=4)
 
-    message = ":rocket: *Nieuwe Scoren met Andy Prijzne* :sunglasses:\n"
+    message = ":rocket: *Nieuwe Scoren met Andy prijzen!* :sunglasses:\n"
     emoji_dict = {"diesel": ":oil_drum:", "Euro 95": ":fuelpump:", "Superplus": ":racing_car:"}
     for fuel, price in fuel_prices.items():
         message += f"{emoji_dict.get(fuel, ':fuelpump:')} *{fuel}*: €{price:.2f}\n"
